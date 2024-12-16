9bda1571708006a419ebaab51cd1554ec4cd1c54
hxxpn <nebojsa.sim2@gmail.com>
2024-12-16 23:40:44 +0100

    Added the original pipnet code as a jumpoff point for module creation

--- pipnet/pipnet.py
+++ pipnet/pipnet.py
@@ -2,5 +2,8 @@ from torch import nn
 
 
 class PIPNet(nn.Module):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self):
         pass
